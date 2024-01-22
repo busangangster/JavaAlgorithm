a85ab0fdbb87b42dd8c3972616b7e8fe15be85cc
busangangster <wlwhssla1034@naver.com>
2024-01-22 23:55:50 +0900

    Baekjoon

--- Baekjoon/자료구조/7662.java
+++ Baekjoon/자료구조/7662.java
@@ -7,39 +7,35 @@ class P7662 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         int t = Integer.parseInt(br.readLine());
+
         for (int i=0; i<t; i++) {
             int k = Integer.parseInt(br.readLine());
-            TreeMap<Integer, Integer> tm = new TreeMap<>();
+            // 수가 들어갈 때마다 그 수의 개수를 세주면서, 키 값에 따라 오름차순으로 정렬하기 위해 TreeMap를 사용
+            TreeMap<Integer, Integer> tm = new TreeMap<>(); 
+
             for (int j=0;j<k;j++) {
                 StringTokenizer st = new StringTokenizer(br.readLine()," ");
                 String s = st.nextToken();
                 char d = s.charAt(0);
                 int v = Integer.parseInt(st.nextToken());
 
-                if (d == 'D') {
+                if (d == 'D') { // 수를 트리에서 빼는 경우
                     if (tm.isEmpty()) continue;
                     else {
                         if (v == 1) {
                             int tmp = tm.lastKey(); // tree의 최댓값
                             int cnt = tm.get(tmp);
-                            if (cnt == 1) {
-                                tm.remove(tmp);
-                            } else {
-                                tm.put(tmp,cnt-1);
-                            }
-
+                            if (cnt == 1) tm.remove(tmp);
+                            else tm.put(tmp,cnt-1);
+                            
                         } else {
                             int tmp = tm.firstKey(); // 트리의 최솟값
                             int cnt = tm.get(tmp);
-                            if (cnt == 1) {
-                                tm.remove(tmp);
-                            } else {
-                                tm.put(tmp,cnt-1);
-                            }
-
+                            if (cnt == 1) tm.remove(tmp);
+                            else tm.put(tmp,cnt-1);
                         }
                     }
-                } else{
+                } else{ // 수를 트리에 집어 넣는경우 
                     tm.put(v,tm.getOrDefault(v,0)+1);
                 }
             }
