be15a2a2600b5612bfe0a7bde29d0b528274eca3
busangangster <wlwhssla1034@naver.com>
2024-01-27 11:53:36 +0900

    Baekjoon

--- Baekjoon/다익스트라/1753.java
+++ Baekjoon/다익스트라/1753.java
@@ -5,8 +5,8 @@ import java.io.*;
 class P1753 {
     static int v,e,start;
     static int INF = Integer.MAX_VALUE;
-    static ArrayList<ArrayList<Node>> graph = new ArrayList<ArrayList<Node>>();
-    static int[] min_dis;
+    static ArrayList<ArrayList<Node>> graph = new ArrayList<ArrayList<Node>>(); // 정점을 저장할 그래프
+    static int[] min_dis; // 최단경로를 저장할 배열
 
     public static void main(String[] args) throws Exception{
         BufferedReader  br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,7 +17,7 @@ class P1753 {
         start = Integer.parseInt(br.readLine());
 
         for (int i=0;i<v+1;i++) {
-            graph.add(new ArrayList<>());
+            graph.add(new ArrayList<>()); // 그래프 초기화
         }
         for (int i=0; i<e;i++) {
             st = new StringTokenizer(br.readLine()," ");
@@ -25,11 +25,12 @@ class P1753 {
             int v = Integer.parseInt(st.nextToken());
             int w = Integer.parseInt(st.nextToken());
             
-            graph.get(u).add(new Node(v,w));               
+            graph.get(u).add(new Node(v,w)); // u -> v로 w의 가중치를 가지는 간선 그리기
         }
 
         dijkstra(start);
-        for (int i=1;i<v+1;i++) {
+
+        for (int i=1;i<v+1;i++) { // 결과값 출력 
             if (min_dis[i] == INF) sb.append("INF").append("\n");
             else sb.append(min_dis[i]).append("\n");
         }
@@ -40,28 +41,29 @@ class P1753 {
 
     public static void dijkstra(int start) {
         min_dis = new int[v+1];
-        for (int i=0;i<v+1;i++) {
+        for (int i=0;i<v+1;i++) { // 각 정점의 최단거리를 MAX_VALUE값으로 초기화
             min_dis[i] = INF;
         }
-        PriorityQueue<Node> q = new PriorityQueue<>((o1,o2) -> o1.cost - o2.cost);
-        min_dis[start] = 0;
-        q.offer(new Node(start,0)); 
+        PriorityQueue<Node> q = new PriorityQueue<>((o1,o2) -> o1.cost - o2.cost); // 우선순위 큐 생성
+
+        min_dis[start] = 0; // 출발지점은 가중치가 0임 
+        q.offer(new Node(start,0));  // Node 클래스에서 정의한 대로 (정점,가중치) 순으로 큐에 삽입 
+
         while (!q.isEmpty()) {
             
             Node cur_node = q.poll();
 
-            if (min_dis[cur_node.idx]< cur_node.cost) {
+            if (min_dis[cur_node.idx]< cur_node.cost) { // 현재 정점까지의 최단경로가 가중치보다 작을 경우는 갱신 필요 X 
                 continue;
             }
-            // cur_node.cost+next_node.cost
-            for (int i =0; i< graph.get(cur_node.idx).size();i++) {
+            
+            for (int i =0; i< graph.get(cur_node.idx).size();i++) { // 해당 정점이랑 간선을 가지는 정점 탐색 
                 Node next_node = graph.get(cur_node.idx).get(i);
-                if (min_dis[next_node.idx] > cur_node.cost + next_node.cost) {
+                if (min_dis[next_node.idx] > cur_node.cost + next_node.cost) { // 최단경로 갱신 
                     min_dis[next_node.idx] = cur_node.cost + next_node.cost;
                     q.offer(new Node(next_node.idx,min_dis[next_node.idx]));
                 }
-            } 
-            
+            }          
         }
     }
 }
--- Baekjoon/정렬/23056.java
+++ Baekjoon/정렬/23056.java
@@ -7,91 +7,59 @@ class P23056 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine()," ");
         StringBuilder sb = new StringBuilder();
+
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        String[][] blue = new String[5000][2];
-        String[][] red = new String[5000][2];
 
-        HashMap<Integer,Integer> map = new HashMap<>();
-        
-        int b = 0;
-        int r = 0;
+        ArrayList<info> blue = new ArrayList<>();
+        ArrayList<info> white = new ArrayList<>();
+        int[] count = new int[n+1];
+
         while (true) {
             st = new StringTokenizer(br.readLine()," ");
-            // ArrayList<String> tmp = new ArrayList<String>();
-            String c = st.nextToken();
-            String s = st.nextToken();
-            if (c.equals("0") && s.equals("0")) break;
+            int c = Integer.parseInt(st.nextToken());
+            String name = st.nextToken();
+
+            if (c == 0 && name.equals("0")) break;
             else {
-                int cc = Integer.parseInt(c);
-                map.put(cc,map.getOrDefault(cc, 0)+1);
-                if (cc % 2==0) {
-                    if (map.get(cc) > m) {
-                        continue;
-                    } else {
-                        red[r][0] = c;
-                        red[r][1] = s;
-                    };
-                    r++;
-                } else {
-                    if (map.get(cc) > m) {
-                        continue;
-                    } else {
-                        blue[b][0] = c;
-                        blue[b][1] = s;
-                    }
-                    b++;
+                if (c % 2 != 0 && count[c] < m) {
+                    blue.add( new info(c,name));
+                    count[c]++;
+                } else if (c % 2 == 0 && count[c] < m) {
+                    white.add(new info(c,name));
+                    count[c]++;
                 }
             }
-
         }
 
-        Arrays.sort(blue,(new Comparator<String[]>() {
-            @Override
-            public int compare(String[] o1, String[] o2) {
-                if (o1[0] != null) {
-                    if (o1[0].equals(o2[0]) && o1[1].length() == o2[1].length()) {
-                        return o1[1].compareTo(o2[1]);
-                    } else if (o1[0].equals(o2[0])) {
-                        return o1[1].length() - o2[1].length();
-                    }
-                } else return 0;
-                return o1[0].compareTo(o2[0]);
-            }
-        }));
+        Collections.sort(blue);
+        Collections.sort(white);
 
-        Arrays.sort(red,(new Comparator<String[]>() {
-            @Override
-            public int compare(String[] o1, String[] o2) {
-                if (o1[0] != null) {
-                    if (o1[0].equals(o2[0]) && o1[1].length() == o2[1].length()) {
-                        return o1[1].compareTo(o2[1]);
-                    } else if (o1[0].equals(o2[0])) {
-                        return o1[1].length() - o2[1].length();
-                    }
-                } else return 0;
-                return o1[0].compareTo(o2[0]);
-            }
-        }));
-
-        for (int i=0;i<5000;i++) {
-            if (blue[i][0] != null) {
-                sb.append(blue[i][0]+" "+blue[i][1]).append("\n");
-            } else {
-                break;
-            }
+        for (int i=0; i<blue.size(); i++) {
+            sb.append(blue.get(i).num + " " + blue.get(i).name).append("\n");
         }
-
-        for (int j=0;j<5000;j++) {
-            if (red[j][0] != null) {
-                sb.append(red[j][0]+" "+red[j][1]).append("\n");
-            } else {
-                break;
-            }
+        for (int i=0; i< white.size(); i++) {
+            sb.append(white.get(i).num + " " + white.get(i).name).append("\n");
         }
-        // System.out.println(Arrays.deepToString(blue));
-        // System.out.println(Arrays.deepToString(red));
-
         System.out.println(sb);
     }    
+}
+
+class info implements Comparable<info>  {
+    int num;
+    String name;
+
+    public info(int num, String name) {
+        this.num = num;
+        this.name = name;
+    }
+
+    @Override
+    public int compareTo(info o) {
+        if (this.num == o.num && this.name.length() == o.name.length()) {
+            return this.name.compareTo(o.name);
+        } else if (this.num == o.num) {
+            return this.name.length() - o.name.length();
+        } return this.num - o.num;
+    }
 }
