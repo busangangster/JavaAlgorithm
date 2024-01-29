9e47172fdeb9acbd387bbf85ee379ada798149b8
busangangster <wlwhssla1034@naver.com>
2024-01-29 23:47:33 +0900

    Baekjoon

--- prac.java
+++ prac.java
@@ -4,19 +4,6 @@ import java.util.*;
 
 class P15927 {
     public static void main(String[] args)throws Exception {
-        int n = 1;
-        int h = 5/2;
-        for (int i =0; i<5;i++) {
-            for (int j =0; i<5-i;j++) {
-                if (i<h) {
-                    System.out.print("   ");
-                }
-                else {
-                    System.out.printf("%3d",n);
-                }
-                if ()
-
-            }
-        }
+        
     }
 }
