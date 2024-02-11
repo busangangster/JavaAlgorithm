60775267a9cb77be24a7b38e038777490e76721e
busangangster <wlwhssla1034@naver.com>
2024-02-11 19:39:45 +0900

    Baekjoon

--- prac.java
+++ prac.java
@@ -3,37 +3,7 @@ import java.io.*;
 import java.util.*;
 
 class prac {
-    public static void main(String[] args)throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int w = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
-
-        int w_t = w/2;
-        int m_t = m/1;
-        int contest= Math.min(w_t,m_t);
-
-        m = m - contest*2;
-        w = w - contest;
-
-        k = (k - (m+w));
-
-        if (k <=0 ) {
-            sb.append(contest);
-        }
-        else {
-            if (k % 3==0) {
-                contest -= k /3;
-            }
-            else {
-                contest -= (k/3 + 1);
-            }
-            sb.append(contest);
-        }
-        System.out.println(sb);
-
+    public static void main(String[] args) throws Exception {
     }
 }
