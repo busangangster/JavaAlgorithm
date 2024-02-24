af7d338b1309c9a560e303acb05dcc65e325a4bf
Busangangster <83652134+busangangster@users.noreply.github.com>
2024-02-24 12:16:07 +0900

    Baekjoon

---
+++ Baekjoon/백트래킹/15656.java
@@ -0,0 +1,44 @@
+package Baekjoon;
+import java.util.*;
+import java.io.*;
+
+class prac {
+   static StringBuilder sb = new StringBuilder();
+   static int N, M;
+   static int[] ans, input;
+
+   public static void main(String[] args) throws Exception {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      StringTokenizer st;
+
+      st = new StringTokenizer(br.readLine());
+      N = Integer.parseInt(st.nextToken());
+      M = Integer.parseInt(st.nextToken());
+
+      input = new int[N];
+      ans = new int[M];
+
+      st = new StringTokenizer(br.readLine());
+      for (int i = 0; i < N; i++) {
+         input[i] = Integer.parseInt(st.nextToken());
+      }
+      Arrays.sort(input);
+      DFS(0);
+      System.out.println(sb);
+   }
+
+   public static void DFS(int cnt) {
+      if (cnt == M) {
+         for (int x : ans) {
+            sb.append(x).append(" ");
+         }
+         sb.append("\n");
+         return;
+      }
+
+      for (int i = 0; i < N; i++) {
+         ans[cnt] = input[i];
+         DFS(cnt + 1);
+      }
+   }
+}
